f7ddc356ab3acafacf78e398ed37703263da98de
Brian Martin <bri365@gmail.com>
2020-06-14 15:01:08 -0500

    testing

--- main.go
+++ main.go
@@ -11,32 +11,59 @@ import (
 )
 
 const (
-	address = "localhost:50505"
+	locationServer = "localhost:50505"
+	storyServer    = "localhost:50506"
 )
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	// Set up a connection to the location server.
+	lsConn, err := grpc.Dial(locationServer, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("Location server did not connect: %v", err)
 	}
-	defer conn.Close()
+	defer lsConn.Close()
 
-	c := pb.NewLocationServiceClient(conn)
+	lsClient := pb.NewLocationServiceClient(lsConn)
 
-	// Contact the server and print out its response.
 	var request pb.LocationRequest
 	request.Longitude = -112.4
 	request.Latitude = 32.4
 	request.Radius = 100000
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	ctx1, cancel1 := context.WithTimeout(context.Background(), time.Second)
+	defer cancel1()
 
-	r, err := c.GetLocations(ctx, &request)
+	r, err := lsClient.GetLocations(ctx1, &request)
 	if err != nil {
 		log.Fatalf("Location service error: %v", err)
 	}
 
 	log.Printf("Locations: %v", r.GetLocations())
+
+	// Set up a connection to the story server.
+	ssConn, err := grpc.Dial(storyServer, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Story server did not connect: %v", err)
+	}
+	defer ssConn.Close()
+
+	cs := pb.NewStoryServiceClient(ssConn)
+
+	// locationIds := [2]string{"loc-123", "loc-456"}
+	var locationIds []string
+	locationIds = append(locationIds, "loc-123")
+	locationIds = append(locationIds, "loc-456")
+
+	var storyRequest pb.StoryRequest
+	storyRequest.LocationIds = locationIds
+
+	ctx2, cancel2 := context.WithTimeout(context.Background(), time.Second)
+	defer cancel2()
+
+	rs, err := cs.GetStories(ctx2, &storyRequest)
+	if err != nil {
+		log.Fatalf("Story service error: %v", err)
+	}
+
+	log.Printf("Stories: %v", rs.GetStories())
 }
